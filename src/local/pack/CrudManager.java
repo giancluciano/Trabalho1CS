d8ebdcff696befa8ccd5d7ddff258d7e160e14e8
Vinicius Gubiani Ferreira <vini.g.fer@gmail.com>
2018-05-31 11:24:37 -0300

    Adicionada aplicacao de demonstracao

---
+++ src/local/pack/CrudManager.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package local.pack;
+
+/**
+ *
+ * @author vinicius
+ */
+public class CrudManager {
+    
+    //New records are always inserted at the end. Returns line number
+    public int insertRecord(String value) {
+        return 0;
+    }
+    
+    public String getRecord(int id) {
+        return "";
+    }
+    
+    public void updateRecord(String newValue, int id) {
+        
+    }
+    
+    public void deleteRecord(int id) {
+    }
+    
+    
+    
+    public void connect(String fileName) {
+    
+    }
+    
+    public void disconnect() {
+        
+    }
+    
+    private void save() {
+    }
+    
+    public String[] getAllRecords() {
+        return new String[0];
+    }
+}
